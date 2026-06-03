18e48fbdbe4ae2bc6f06307e5575cfa0c5dec18b
Marfidha <marfidhaap@gmail.com>
2026-06-03 15:11:40 +0530

    update frontend url

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,8 +3,7 @@ import nodemailer from "nodemailer"
 
      // Create a transport with your SMTP server settings
 const transporter=nodemailer.createTransport({
-     host: "smtp.gmail.com",
-        port: 587,
+    service: "gmail",
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
